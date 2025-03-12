33d1d3c6eeb296c19dd86413a56c64b2ba0a8b81
Umar Ali <umarali@Umars-MacBook-Pro.local>
2025-03-12 12:21:48 +0530

    Added new variables

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@
 data "template_file" "test" {
   template = "Hello ${var.name}!"
 }
+
+data "template_file" "test2" {
+  template = "Hello ${var.v1}!"
+}
+
+data "template_file" "test3" {
+  template = "Hello ${var.v2}!"
+}
--- vars.tf
+++ vars.tf
@@ -4,3 +4,13 @@ variable "name" {
   description = "The name to pass to the template."
   default     = "world"
 }
+
+variable "v1" {
+  description = "The name to pass to the template."
+  default     = "v1"
+}
+
+variable "v2" {
+  description = "The name to pass to the template."
+  default     = "v2"
+}
