7d0bf1fd1eba2900d1fd9dcdb58c4932e712f5aa
Umar Ali <umarali@Umars-MacBook-Pro.local>
2025-03-12 12:28:25 +0530

    removed variables

--- main.tf
+++ main.tf
@@ -2,11 +2,3 @@
 data "template_file" "test" {
   template = "Hello ${var.name}!"
 }
-
-data "template_file" "test2" {
-  template = "Hello ${var.v1}!"
-}
-
-data "template_file" "test3" {
-  template = "Hello ${var.v2}!"
-}
--- vars.tf
+++ vars.tf
@@ -5,12 +5,3 @@ variable "name" {
   default     = "world"
 }
 
-variable "v1" {
-  description = "The name to pass to the template."
-  default     = "v1"
-}
-
-variable "v2" {
-  description = "The name to pass to the template."
-  default     = "v2"
-}
